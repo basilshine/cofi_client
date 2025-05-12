ade2a99e1abf2578af7a0e2d3cff605c557b5944
vasiliy.enshin <vasiliy.enshin@promwad.com>
2025-05-12 12:12:32 +0700

    Add dist directory to .gitignore, update package.json to include gh-pages for deployment, and modify vite.config.ts to use node:path for improved compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/cofi-client/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@contexts': path.resolve(__dirname, './src/contexts'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@providers': path.resolve(__dirname, './src/providers'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-      },
-    },
-  },
-}); 
+	plugins: [react()],
+	base: process.env.NODE_ENV === "production" ? "/cofi_client/" : "/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@hooks": path.resolve(__dirname, "./src/hooks"),
+			"@store": path.resolve(__dirname, "./src/store"),
+			"@types": path.resolve(__dirname, "./src/types"),
+			"@utils": path.resolve(__dirname, "./src/utils"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@contexts": path.resolve(__dirname, "./src/contexts"),
+			"@services": path.resolve(__dirname, "./src/services"),
+			"@providers": path.resolve(__dirname, "./src/providers"),
+		},
+	},
+	server: {
+		proxy: {
+			"/api": {
+				target: process.env.VITE_API_URL,
+				changeOrigin: true,
+			},
+		},
+	},
+});
