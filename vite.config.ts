1a01fb5fe948f6b7b826b94ebc6aac9d061dc39d
vasiliy.enshin <vasiliy.enshin@promwad.com>
2025-05-16 22:47:01 +0700

    Update Vite configuration to set base path for production environment to root. This change simplifies the deployment structure by ensuring the base path is consistent across environments.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
-	base: process.env.NODE_ENV === "production" ? "/cofi_client/" : "/",
+	base: process.env.NODE_ENV === "production" ? "/" : "/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
